76552cf6884c121ab85b47255725946ed263c798
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2026-05-09 16:27:18 +0100

    feat: add FeaturedDestination component to showcase selected hotels

---
+++ client/src/components/FeaturedDestination.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { roomsDummyData } from '../assets/assets'
+import HotelCard from './HotelCard'
+import Title from './Title'
+import { useNavigate } from 'react-router-dom'
+
+const FeaturedDestination = () => {
+  const navigate = useNavigate()
+  return (
+    <div className='flex flex-col items-center px-2 md:px-16 lg:px-24 bg-slate-100 py-20'>
+
+        <Title title="Featured Hotels" subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences"/>
+        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
+            {roomsDummyData.slice(0, 4).map((room, index) => (
+                <HotelCard key={room._id} room={room} index={index} />
+            ))}
+        </div>
+
+        <button onClick={()=> {navigate('/rooms'); scrollTo(0,0)}} 
+        className='my-16 py-3 px-4 text-sm text-gray-500 border border-gray-300 bg-white font-medium rounded hover:bg-gray-50 transition-all cursor-pointer'>
+            View All Destinations
+        </button>
+    </div>
+  )
+}
+
+export default FeaturedDestination
