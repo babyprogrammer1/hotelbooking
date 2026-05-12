aab0cc4c0e10ef4ada5079c171dcbc989921e630
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2026-05-12 15:49:17 +0100

    feat: update App component layout and fix owner path check

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,16 +2,20 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import Footer from './components/Footer'
 
 const App = () => {
 
-  const isOwnerPath = useLocation().pathname.startsWith('/owner');
+  const isOwnerPath = useLocation().pathname.includes('owner');
   return (
     <div>
       {!isOwnerPath && <Navbar />}
+      <div className='min-h-[70vh]'>
       <Routes>
         <Route path='/' element={<Home />} />
       </Routes>
+      </div>
+      <Footer />
     </div>
   )
 }
