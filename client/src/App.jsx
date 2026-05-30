20684ab2b7d13a85a3ee3559515fa94a63e03391
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2026-05-30 11:38:17 +0100

    feat: add routes for hotel owner management in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,11 @@ import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
 import HotelReg from './components/HotelReg'
+import Layout from './pages/hotelOwner/Layout'
+import AddRoom from './pages/hotelOwner/AddRoom'
+import ListRooms from './pages/hotelOwner/ListRooms'
+import Dashboard from './pages/hotelOwner/Dashboard'
+
 
 const App = () => {
 
@@ -21,6 +26,11 @@ const App = () => {
         <Route path='/rooms' element={<AllRooms />} />
         <Route path='/rooms/:id' element={<RoomDetails />} />
         <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/owner' element={<Layout/>}>
+          <Route index element={<Dashboard />} />
+          <Route path='add-room' element={<AddRoom />} />
+          <Route path='list-room' element={<ListRooms />} />
+        </Route>
       </Routes>
       </div>
       <Footer />
