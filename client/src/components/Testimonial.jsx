84cd8c7d19552359f96430ea96edf1fb02e827c8
Rafiu Ayomide <rafiuayomide1811@gmail.com>
2026-05-30 13:34:27 +0100

    fix: adjust layout in Testimonial component for consistent styling

--- client/src/components/Testimonial.jsx
+++ client/src/components/Testimonial.jsx
@@ -7,7 +7,7 @@ const Testimonial = () => {
   return (
     <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-100 pt-20 pb-30'>
         <Title title="What Our Guests Say" subTitle="Discover why discerning travelers choose QuickStay for their luxury accommodations around the world." />
-        <div className="flex flex-wrap items-center gap-6 mt-20">
+        <div className="flex items-center gap-6 mt-20">
                 {testimonials.map((testimonial) => (
                     <div key={testimonial.id} className="bg-white p-6 rounded-xl shadow ">
                         <div className="flex items-center gap-3">
