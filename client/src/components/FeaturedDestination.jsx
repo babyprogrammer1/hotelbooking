d2c2e5eaf491a2af8e709103aa835d4a0d4a1422
Rafiu Ayomide Aishat <rafiuayomide1811@gmail.com>
2026-05-30 20:12:54 +0100

    Update layout for FeaturedDestination component

--- client/src/components/FeaturedDestination.jsx
+++ client/src/components/FeaturedDestination.jsx
@@ -10,7 +10,7 @@ const FeaturedDestination = () => {
     <div className='flex flex-col items-center px-2 md:px-16 lg:px-24 bg-slate-100 py-20'>
 
         <Title title="Featured Hotels" subTitle="Discover our handpicked selection of exceptional properties around the world, offering unparalleled luxury and unforgettable experiences"/>
-        <div className='flex items-center justify-center gap-6 mt-20'>
+        <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
             {roomsDummyData.slice(0, 4).map((room, index) => (
                 <HotelCard key={room._id} room={room} index={index} />
             ))}
